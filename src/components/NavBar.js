a64a28f76dcbce9dfc39e78aa6ccaa16a48a081c
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-08-21 15:05:01 -0700

    lab complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((link) => {
+    return (
+      <a href={`#${link}`} key={link}>
+        {link}
+      </a>
+    );
+  });
+  return <nav>{linkElements}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologySpan = technologies.map((technology, index) => {
+    return <span key={index}>{technology}</span>;
+  });
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{technologySpan}</div>
     </div>
   );
 }
